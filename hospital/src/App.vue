c5136bdcc1fe3afe052435801dca89de26b45adc
Your Name <greenfitter516@gmail.com>
2021-10-01 20:33:10 +0600

    revise functionality

--- hospital/src/App.vue
+++ hospital/src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h3> {{ title }}</h3>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+
+    return {
+        title:'United Hospital,Dhaka'
+    }
+
   }
+  
 }
 </script>
 
@@ -22,7 +28,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color:#003366;
   margin-top: 60px;
 }
 </style>
