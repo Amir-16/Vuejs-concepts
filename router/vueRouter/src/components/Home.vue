e04eddcd7dfd17a0b9ce63692408a51a3404cff5
Your Name <greenfitter516@gmail.com>
2021-10-15 11:26:57 +0600

    vueRouter

---
+++ router/vueRouter/src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+
+
+<div>
+
+  <h3>
+     Vue Js Journey
+  </h3>
+</div>
+</template>
+
+<script>
+export default {
+
+    name:'Home',
+    data(){
+
+      return{
+
+
+      }
+    }
+
+}
+</script>
+
+
+<style>
+
+</style>
